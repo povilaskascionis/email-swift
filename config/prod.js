873c8c623dcff5c692f12c564d2f3b25c9a8f354
Povilas Kaščionis <p.kascionis@adform.com>
2017-08-17 11:27:14 +0300

    fix prod.js variables being strings

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 //Commit me!
 
 module.exports = {
-  googleClientID: 'process.env.GOOGLE_CLIENT_ID',
-  googleClientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
-  mongoURL: 'process.env.MONGO_URL',
-  cookieKey: 'process.env.COOKIE_KEY'
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURL: process.env.MONGO_URL,
+  cookieKey: process.env.COOKIE_KEY
 };
