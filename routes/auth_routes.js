0218863b1690b40372cfcee3886957609b05e01c
Povilas Kaščionis <p.kascionis@adform.com>
2017-08-30 10:43:04 +0300

    remove root route handler on backend

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,10 +1,6 @@
 const passport = require('passport');
 
 module.exports = app => {
-  app.get('/', (req, res) => {
-    res.send({ heyHey: 'banana' });
-  });
-
   app.get(
     '/auth/google',
     passport.authenticate('google', {
